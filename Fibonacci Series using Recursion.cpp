d9f6480aee27b1d19da6a98bf0f7cb9d505997cf
Harsh <jhaharsh101@gmail.com>
2023-08-23 22:24:34 +0530

    Fibonnaci series using recursion logic comment upadted

--- Fibonacci Series using Recursion.cpp
+++ Fibonacci Series using Recursion.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int fib(int n) {
-	
+	//2 conditions as calling function for n-1 and n-2, so to keep a check it does not go in negative a check of n==0 and n==1 so on function call 3 it call 2 and 1 which will be delivered from below check
 	if(n==0) {
 		return 0;
 	} else if (n==1) {
